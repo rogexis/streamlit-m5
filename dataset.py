4baace1b80169d0bcbc77fed4dbf058721b2c321
rogexis <81534992+rogexis@users.noreply.github.com>
2023-03-22 02:21:00 +0000

    add dataset

--- dataset.py
+++ dataset.py
@@ -1,8 +1,16 @@
 import streamlit as st 
 import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 names_link = 'dataset.csv'
 names_data = pd.read_csv(names_link)
 st.title ("Read CSV")
 
 st.dataframe(names_data)
+
+fig, ax = plt.subplots()
+
+sns.countplot(x=names_data.sex, ax=ax)
+
+st.pyplot(fig)
